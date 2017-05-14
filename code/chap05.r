38db24b82082671c7c599c08ffc640093d454271
nevrome <clemens@nevrome.de>
2017-05-14 22:27:58 +0200

    optimized script for chapter 5 to run out of the box

--- code/chap05.r
+++ code/chap05.r
@@ -23,16 +23,15 @@
 ############################################## #
 
 # 0. Preparation ===============================
-#  working direktory
-wd <- "~/modproj_qaam"
-wd <- "/home/fon/daten/analyse/modproj_qaam"
-setwd(wd)
-load("4ws/ws04.rws")
+# load data 
+
+# this image is created by the chap04 script
+load("ws/ws04.rws")
 
 # 1. Regression ================================
 library(gdata) 
 
-file_vil    <- "1data/villages.xls" 
+file_vil    <- "data/villages.xls" 
 df_vil <- read.xls(file_vil)
 # Correlation
 X <- df_vil[,3]
@@ -40,7 +39,7 @@ E_vil <- df_vil[,1]
 
 cor(cbind(X,E_vil), method="pearson")
 # Regression T1, T2
-pdf("6pictures/c5_T1T2vil.pdf", height=4, width=6, bg = "white") 
+pdf("pictures/c5_T1T2vil.pdf", height=4, width=6, bg = "white") 
     plot(X,E_vil,col="black", pch=16)
     lines(c(df_vil[1,3],df_vil[13,3]),c(df_vil[1,1], df_vil[13,1]) ) 
     years <- 1259:1350
@@ -61,7 +60,7 @@ plot(X- 1259,E_vil,col="black", pch=16)
 abline(T3_vil)
 
 
-pdf("6pictures/c5_T3vil.pdf", height=4, width=6, bg = "white") 
+pdf("pictures/c5_T3vil.pdf", height=4, width=6, bg = "white") 
     plot(X- 1259,E_vil,col="black", pch=16)
     abline(T3_vil)
 dev.off() 
@@ -83,7 +82,7 @@ fT5_vil <- fitted(T5_vil)
 sum(((E-fT5_vil)^2)/fT5_vil)
 summary(T5_vil)$r.squared
 
-pdf("6pictures/c5_T5vil.pdf", height=4, width=6, bg = "white") 
+pdf("pictures/c5_T5vil.pdf", height=4, width=6, bg = "white") 
     plot(X- 1259,E_vil,col="black", pch=16)
     lines(yr,fT5_vil)
 dev.off() 
@@ -95,7 +94,7 @@ for(i in seq(1:20)){
     chi[i] <- sum(((E-fT6_vil)^2)/fT6_vil)
 }
 chi
-pdf("6pictures/c5_chi.pdf", height=4, width=6, bg = "white") 
+pdf("pictures/c5_chi.pdf", height=4, width=6, bg = "white") 
     plot(chi,col="black", pch=16)
     lines(chi)
 dev.off() 
@@ -107,7 +106,7 @@ for(i in seq(1:20)){
     r[i] <- summary(T7_vil)$r.squared
 }
 r
-pdf("6pictures/c5_r.pdf", height=4, width=6, bg = "white") 
+pdf("pictures/c5_r.pdf", height=4, width=6, bg = "white") 
     plot(r,col="black", pch=16)
     lines(r)
 dev.off() 
@@ -134,7 +133,7 @@ for (d in 1:dd) {
     eov <- mean(eo)
     res <- rbind(res,data.frame(d=d,eiv=eiv,eov=eov))
 }
-pdf("6pictures/c5_boot.pdf", height=4, width=6, bg = "white") 
+pdf("pictures/c5_boot.pdf", height=4, width=6, bg = "white") 
     plot(res$d,res$eiv,col="grey",log="y",ylim=c(min(res$eiv),max(res$eov)), pch=16, type="b")
     points(res$d,res$eov, type ="b", pch=16, col="black")
 dev.off() 
@@ -153,7 +152,7 @@ E8 <- E3-pred8
 T9_vil <- lm(E8 ~ poly(yr, 12, raw=TRUE))
 fT9_vil <- fitted(T9_vil)
 
-pdf("6pictures/c5_decomp.pdf", height=4, width=6, bg = "white") 
+pdf("pictures/c5_decomp.pdf", height=4, width=6, bg = "white") 
     plot(X- 1259,E_vil,col="white", pch=16, ylim=c(-2,12))
     lines(yr,fT3_vil, col="grey")
     curve(coef(T8_vil)[1] + coef(T8_vil)[2]*x + coef(T8_vil)[3]*x^2 + coef(T8_vil)[4]*x^3 + coef(T8_vil)[5]*x^4, add = TRUE)
@@ -172,6 +171,7 @@ image(meg_idw, col = gray.colors(25, start = 0.4, end = 0.97))
 contour(meg_idw, add=T) 
 points(ppp_meg$x, ppp_meg$y, pch=16, cex=0.4) 
 
+library(rgeos)
 library(maptools)
 library(spatstat)
 rw <- 500  
@@ -180,17 +180,17 @@ dens_r <- density(ppp_meg, sd, eps=rw, edge=TRUE, at="pixels")
 #plot(dens_r, col = gray.colors(25, start = 0.97, end = 0.4))
 df_dens_r <- as.SpatialGridDataFrame.im(dens_r)
 meg_kde_samppoints <- spsample(df_dens_r, 1000,  type="random")
-meg_kde_samp <- overlay(x=df_dens_r, y= meg_kde_samppoints)
-##### meg_kde_samp <- over(meg_kde_samppoints, df_dens_r)
-vt2    <- variogram(meg_kde_samp@data$v ~ 1, meg_kde_samp)
+#meg_kde_samp <- overlay(x=df_dens_r, y= meg_kde_samppoints)
+meg_kde_samp <- sp::over(meg_kde_samppoints, df_dens_r)
+vt2    <- variogram(meg_kde_samp$v ~ 1, meg_kde_samppoints)
 v.fit2 <- fit.variogram(vt2, vgm(1, "Gau", 5000, 1), fit.sills = TRUE, fit.ranges = TRUE, fit.method = 7)
 plot(vt2,v.fit2)
-k2     <- krige(meg_kde_samp@data$v ~ 1, meg_kde_samp, df_dens_r, v.fit2, nmin = 3, maxdist = 10000, nmax = 8)
+k2     <- krige(meg_kde_samp$v ~ 1, meg_kde_samppoints, df_dens_r, v.fit2, nmin = 3, maxdist = 10000, nmax = 8)
 image(k2, col = gray.colors(25, start = 0.97,  end = 0.4))
 contour(k2, add=T)    
 points(ppp_meg$x, ppp_meg$y, pch=16, cex=0.4) 
 
-save.image("4ws/ws05.rws")
+save.image("ws/ws05.rws")
 
 
 
